771db0d7561a0d32b525db4b14766353c158bdad
Richa Dubey <richadubey39@gmail.com>
2024-03-29 20:08:39 +0530

    Add the changes for survery and calculate the average survery

--- questionnaire.rb
+++ questionnaire.rb
@@ -1,7 +1,7 @@
-require "pstore" # https://github.com/ruby/pstore
+require "pstore"
 
 STORE_NAME = "tendable.pstore"
-store = PStore.new(STORE_NAME)
+@store = PStore.new(STORE_NAME)
 
 QUESTIONS = {
   "q1" => "Can you code in Ruby?",
@@ -11,18 +11,64 @@ QUESTIONS = {
   "q5" => "Can you code in C#?"
 }.freeze
 
-# TODO: FULLY IMPLEMENT
-def do_prompt
-  # Ask each question and get an answer from the user's input.
-  QUESTIONS.each_key do |question_key|
-    print QUESTIONS[question_key]
-    ans = gets.chomp
+def start_survey
+  puts "Welcome to the coding skills survey!"
+  total_rating = 0
+  total_surveys = 0
+  all_ratings = []
+
+  loop do
+    total_surveys += 1
+    puts "Starting Survey #{total_surveys}:"
+    survey_rating = run_survey
+    all_ratings << survey_rating
+    total_rating += survey_rating
+
+    average_rating = total_rating.to_f / total_surveys
+    puts "Average rating for all surveys: #{average_rating.round(2)}%"
+    
+    break unless start_again?
+  end
+  
+  overall_average_rating = all_ratings.inject(0.0) { |sum, rating| sum + rating } / all_ratings.size
+  puts "Overall average rating across all surveys: #{overall_average_rating.round(2)}%"
+end
+
+def run_survey
+  answers = {}
+
+  QUESTIONS.each do |question_key, question|
+    print "#{question} (Yes/No): "
+    answer = gets.chomp.downcase
+    until ['yes', 'no'].include?(answer)
+      print "Please answer with Yes or No: "
+      answer = gets.chomp.downcase
+    end
+    answers[question_key] = answer == 'yes'
+  end
+
+  store_survey(answers)
+  calculate_rating(answers)
+end
+
+def store_survey(answers)
+  @store.transaction do
+    @store[:runs] ||= []
+    @store[:runs] << answers
   end
 end
 
-def do_report
-  # TODO: IMPLEMENT
+def calculate_rating(answers)
+  total_questions = QUESTIONS.size
+  correct_answers = answers.count { |_, answer| answer }
+  rating = (correct_answers.to_f / total_questions) * 100
+  rating.round(2)
 end
 
-do_prompt
-do_report
+def start_again?
+  puts "Do you want to start another survey? (Yes/No)"
+  response = gets.chomp.downcase
+  response == 'yes'
+end
+
+start_survey
